b23df5d351ad2425dda4ed72bf7f53b64aaa9885
Manish7830 <ms2500287@gmail.com>
2026-07-09 17:37:09 +0530

    Update nginx

--- hello.sh
+++ hello.sh
@@ -5,4 +5,6 @@ echo "Current Directory : $(pwd)"
 echo "Date : $(date)"
 echo "Update : Updateing the list"
 sudo apt-get update
+sudo apt install -y nginx
+nginx --version
 echo "Package update completed  "
